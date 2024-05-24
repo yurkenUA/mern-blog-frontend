d1a5f69788ac9d22ae8db121f84608038bd3fec5
Yurii Zatoka <yuriy.zatoka@gmail.com>
2024-05-24 15:12:01 -0700

    React Router & Redux TK was added, redux store created

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,42 +3,43 @@ import Button from '@mui/material/Button';
 
 import styles from './Header.module.scss';
 import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
-  const isAuth = false;
+	const isAuth = false;
 
-  const onClickLogout = () => {};
+	const onClickLogout = () => {};
 
-  return (
-    <div className={styles.root}>
-      <Container maxWidth="lg">
-        <div className={styles.inner}>
-          <a className={styles.logo} href="/">
-            <div>ARCHAKOV BLOG</div>
-          </a>
-          <div className={styles.buttons}>
-            {isAuth ? (
-              <>
-                <a href="/posts/create">
-                  <Button variant="contained">Написать статью</Button>
-                </a>
-                <Button onClick={onClickLogout} variant="contained" color="error">
-                  Выйти
-                </Button>
-              </>
-            ) : (
-              <>
-                <a href="/login">
-                  <Button variant="outlined">Войти</Button>
-                </a>
-                <a href="/register">
-                  <Button variant="contained">Создать аккаунт</Button>
-                </a>
-              </>
-            )}
-          </div>
-        </div>
-      </Container>
-    </div>
-  );
+	return (
+		<div className={styles.root}>
+			<Container maxWidth="lg">
+				<div className={styles.inner}>
+					<Link className={styles.logo} to="/">
+						<div>ZATOKA BLOG</div>
+					</Link>
+					<div className={styles.buttons}>
+						{isAuth ? (
+							<>
+								<Link to="/posts/create">
+									<Button variant="contained">Написать статью</Button>
+								</Link>
+								<Button onClick={onClickLogout} variant="contained" color="error">
+									Выйти
+								</Button>
+							</>
+						) : (
+							<>
+								<Link to="/login">
+									<Button variant="outlined">Login</Button>
+								</Link>
+								<Link to="/register">
+									<Button variant="contained">Create an account</Button>
+								</Link>
+							</>
+						)}
+					</div>
+				</div>
+			</Container>
+		</div>
+	);
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
 
 import './index.scss';
 import { ThemeProvider } from '@mui/material';
 import { theme } from './theme';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -13,7 +16,11 @@ root.render(
 	<React.StrictMode>
 		<CssBaseline />
 		<ThemeProvider theme={theme}>
-			<App />
+			<BrowserRouter>
+				<Provider store={store}>
+					<App />
+				</Provider>
+			</BrowserRouter>
 		</ThemeProvider>
 	</React.StrictMode>,
 );
