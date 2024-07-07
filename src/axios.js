394529a66e8100909c56cf1be306ad841d73f8a0
Yurii Zatoka <yuriy.zatoka@gmail.com>
2024-07-06 23:57:17 -0700

    add render.com deployed baseURL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-	baseURL: 'http://localhost:4444/',
+	// baseURL: 'http://localhost:4444/',
+	baseURL: 'https://blog-mern-ows8.onrender.com/',
 });
 
 instance.interceptors.request.use((config) => {
